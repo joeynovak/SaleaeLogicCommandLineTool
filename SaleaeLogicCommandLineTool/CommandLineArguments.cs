d35bdf00c0cfeabb462755f82d1e101cbaa170cc
joey <joey.novak@gmail.com>
2016-12-19 13:31:18 -0700

    Initial Commit

---
+++ SaleaeLogicCommandLineTool/CommandLineArguments.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace SaleaeLogicCommandLineTool
+{
+   class CommandLineArguments
+   {
+      [Option('a', "action", Required = true, HelpText = "Action you wish to perform.  Options are: Start, Stop, Save, Load")]
+      public string Action { get; set; }
+
+      [Option('v', "verbose", HelpText = "Prints all messages to standard output.")]
+      public bool Verbose { get; set; }
+
+      [Option('f', "filename", HelpText = "Filename to save captures to.")]
+      public string Filename { get; set; }
+
+      [HelpOption]
+      public string GetUsage()
+      {
+         return HelpText.AutoBuild(this,
+           (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+      }
+
+      public static CommandLineArguments Get(string[] args)
+      {
+         CommandLineArguments commandLineArguments = new CommandLineArguments();
+         Parser.Default.ParseArguments(args, commandLineArguments);
+         return commandLineArguments;
+      }
+   }
+}
---
+++ SaleaeLogicCommandLineTool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Saleae.SocketApi;
+
+namespace SaleaeLogicCommandLineTool
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         CommandLineArguments arguments = CommandLineArguments.Get(args);
+         SaleaeClient client = new SaleaeClient();
+         client.WaitForResponse = false;
+         switch (arguments.Action.ToLower())
+         {
+            case "start":
+               client.Capture();      
+               break;
+            case "stop":
+               client.StopCapture();
+               break;
+            case "save":
+               client.SaveToFile(arguments.Filename);
+               break;
+            case "load":
+               client.LoadFromFile(arguments.Filename);
+               break;
+            default:
+               Console.WriteLine("Unknown action: " + arguments.Action);
+               break;
+         }                  
+         client.Close();
+      }      
+   }
+}
